26ad77e623505fc14d38ec0b2794c48fd2712e86
petrero <petrero@gmail.com>
2025-12-14 20:56:29 +0200

    22.4 Okay, and so let's go see if we can go and wire up these two buttons to pose and play the animation at the top here. Let's go cones, play button, document query selector. And we want the ID of play. And yeah, let's have that as an HTML button element and we'll do the same with the pause button. And as you can see, uh, copilot is hallucinating a third reverse button. We might keep it because that might actually be a fun idea.  Alright, so we got play button and pause button. Let's scroll down here. And we are going to add event listeners to these buttons. So co-pilot is going to figure it out as soon as I type one. Letter play, button add event listener, click on click. We want to trigger the animation, do play. And on pause we want to trigger the animation that pause. And this is self-explanatory. Whenever we click on the play button, we play the animation and vice versa. So let's try to pause at a given time now and it's pause and then play. And it goes back. Pause, play, pause, play, pause, play.

--- resources/js/js-animation.ts
+++ resources/js/js-animation.ts
@@ -21,16 +21,27 @@ const keyframes = [
   },
   { transform: 'translateX(0px) translateY(0px)' }
 ]
-
 const options: KeyframeAnimationOptions = {
   duration: 5000,
   iterations: Infinity,
   easing: 'ease-in-out'
 }
+const playButton = document.querySelector('#play') as HTMLButtonElement
+const pauseButton = document.querySelector('#pause') as HTMLButtonElement
 
 const animation = square.animate(keyframes, options)
 
-// animation.pause()
+playButton.addEventListener('click', () => {
+  animation.play()
+})
+
+pauseButton.addEventListener('click', () => {
+  animation.pause()
+})
+
+reverseButton.addEventListener('click', () => {
+  animation.reverse()
+})
 
 /*square.style.transform = 'rotate(45deg)'
 
