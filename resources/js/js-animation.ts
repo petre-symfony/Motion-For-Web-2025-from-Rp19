1d237564e2667b99eae7f6c1de6787f28864efef
petrero <petrero@gmail.com>
2025-12-14 15:32:43 +0200

    19.7 Now let's say we wanted to stop that animation when it reaches 45 degrees. So cons MAX_ROTATION equals 45. Now when you use set interval, it's going to allocate some memory and it's going to return you a number id. So we can grab that cons interval ID and we will let, uh, copilot fix the spelling. Thank you. And so now this ID allows me to do the opposite of set interval, which is clear interval when you want to stop this function from running and using memory. So we could keep our logic here and then here go if the rotation exceeds or matches the max rotation. Let's clear interval and pass the ID that we got from the set interval here. So we set an interval where we increase the rotation, but when the rotation has matched the max rotation, we clear that interval, which should stop the rotation from happening. And so the fact that the square is now stopped at 45 degree rotation angle tells me that it's working already.

--- resources/js/js-animation.ts
+++ resources/js/js-animation.ts
@@ -3,8 +3,12 @@ const square = document.querySelector('.square')
 square.style.transform = 'rotate(45deg)'
 
 let rotation = 0
+const MAX_ROTATION = 45
 
-setInterval(() => {
+const intervalId = setInterval(() => {
   rotation += 1
   square.style.transform = `rotate(${rotation}deg)`
+  if (rotation >= MAX_ROTATION) {
+    clearInterval(intervalId)
+  }
 }, 1000/60)
