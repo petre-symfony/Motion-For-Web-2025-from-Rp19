fba561a8db641e3d1011a62970356e4cdca7a528
petrero <petrero@gmail.com>
2025-12-14 18:54:25 +0200

    21.6 Okay, so let's change the iterations counts to the keyword infinity, which will run forever. And I promise you that there was a way to define what percentage of the animation these elements should be.  So these, for example should be 0%, 25, 50, 75 and a hundred like copilot worked out. But what if we wanted this one to be 40% instead then this one 50 and then this one 90% and then a hundred percent. Okay, so check this out. In each key frame we can define any CSS properties that we wanted. We are always doing just one here, but we could have border solid eight pixels black. And so now surprise, surprise, a eight pixel blackboard would appear at the second key frame and then animate back out.

--- resources/js/js-animation.ts
+++ resources/js/js-animation.ts
@@ -1,16 +1,16 @@
 const square = document.querySelector('.square') as HTMLDivElement
 
 const keyframes = [
-  { transform: 'translateX(0px) translateY(0px)' },
-  { transform: 'translateX(100px)' },
-  { transform: 'translateX(100px) translateY(100px)' },
-  { transform: 'translateX(0px) translateY(100px)' },
-  { transform: 'translateX(0px) translateY(0px)' }
+  { transform: 'translateX(0px) translateY(0px)' }, //0%
+  { transform: 'translateX(100px)', border: 'solid 8px black' }, //40%
+  { transform: 'translateX(100px) translateY(100px)' }, //50%
+  { transform: 'translateX(0px) translateY(100px)' }, //90%
+  { transform: 'translateX(0px) translateY(0px)' } //100%
 ]
 
 const options: KeyframeAnimationOptions = {
   duration: 2000,
-  iterations: 3,
+  iterations: Infinity
 }
 
 square.animate(keyframes, options)
