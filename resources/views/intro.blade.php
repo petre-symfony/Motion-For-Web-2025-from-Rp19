55d55fd613f41d8acbb7c81756ba2e4d37ca97d0
petrero <petrero@gmail.com>
2025-12-14 20:48:56 +0200

    22.3 So that part you can also do with CSS. Actually I don't think I've showed that to you, but there is an animation play state properly that you can set to playing or pause. Here it is. And of course I got it wrong, it's paused or running.  So if the animation is running, it's running and you can pause it at any time and then run it again. And I guess that's what we're doing here. Let's add a section in our template. I'll wrap them in a div and we'll have a button and yeah, let's go with this pose and resume, but I'll change this to play. And so here are the buttons, uh, getting in the way of the animation. So let's add a class here and let's go in the JS animation. That CSS. So that controls margin top and the animation goes down by a hundred pixels. So go one 50 pixels and then we can nest in here. The button elements. Should we go with copilot suggestion? Why not? This is, why not. It looks terrible. All right, background of C seven, C seven, C seven. Then padding 0.5 in one rem. And let's make the parent a display flex with a gap of one rem. There you go. Still terrible, but a little bit less arguably.

--- resources/views/intro.blade.php
+++ resources/views/intro.blade.php
@@ -1,3 +1,8 @@
 <x-layout title="Animation loops in JS" stylesheet="js-animation.css" script="js-animation.ts">
   <div class="square"></div>
+
+  <div class="controls">
+    <button id="pause">Pause</button>
+    <button id="play">Play</button>
+  </div>
 </x-layout>
