2afe09e5d221e71fb6a6c13a6565af9066c568f1
petrero <petrero@gmail.com>
2025-12-14 16:00:45 +0200

    20.2 But basically on that graph, Jake shows the disparity or discrepancy between what the browser rendering can do and then the task that we put in the loop, just like the set timeout, uh, set interval, sorry.  Sometimes, uh, we are telling to execute some code every, like we said, 60 frame per second every 16 milliseconds. But it's going to go in the loop that skips the rendering because the browser is not capable and ready to perform a rendering or a paint. And so there's a lot of loss of extra effort and sometimes also we skip frames because we are not in sync.  And so this talk then introduce the concept that we're going to talk in this lesson, which is request animation frame. So let's comment this out here and we are going to write this in a different manner. We'll have a function called rotate square and so in there we will do the same that we were doing. So we'll increments the rotation angle and then apply the transform and then watch this. Here we are going to request animation frame, which is essentially a request to the browser to run some code only when the next iteration of an animation frame or a repaint is happening in the rendering engine. And so you can see the squiggly is here because it's expecting something to run when the next animation frame is available. And so here we are going to pass recursively the rotate square function. We're gonna call itself whenever the browser is ready to run it again. So we've defined that function, but we need to actually call it initially for the first time. So let's do it right here.  Notice that there is no concept of, uh, refresh rate or when to rerun the animation here. Instead, we are offloading this to the browser that can make the decision based on its capability, the device capability of when it should rerun to synchronize with the browser's rendering. And so it looks like our animation once again is running smoothly, but this time it's synchronized with the browser. We haven't set it to 60 frame per second or whatever we wanted. We basically say, Hey, I want the most, uh, fluid and smooth animation based on what you're capable of doing. Please go ahead and then request animation framed. That's just that it calls itself every time that there is a rendering cycle about to happen. And so back to Jake's talk, you can see that we had a set interval on the left here, uh, and sometimes it was out of sync with what the rendering engine can do, but request animation frame sits right with the rendering behavior of the browser. And so it's always going to be in sync. And so whenever that door is closed here, uh, the loop is not running in the void. It's always going to be only operated whenever it makes sense and the browser can do, which leads to not skipping frames, not having extra loops where we do work for nothing and so much more performance animations. Also, when you are away from this tab in the browser and visiting something else, uh, the browser is able to say, Hey, we don't need to run this 60 times per second. Nobody's looking at it. And so it can sort of pose or slow down the animation frequency, which has impact on the device battery, uh, electricity bill and a lot of things. So request animation frame is much more performant and recommended than using set interval.

--- resources/js/js-animation.ts
+++ resources/js/js-animation.ts
@@ -5,10 +5,19 @@ square.style.transform = 'rotate(45deg)'
 let rotation = 0
 const MAX_ROTATION = 45
 
-const intervalId = setInterval(() => {
+function rotateSquare() {
+  rotation += 1
+  square.style.transform = `rotate(${rotation}deg)`
+
+  requestAnimationFrame(rotateSquare)
+}
+
+rotateSquare()
+
+/*const intervalId = setInterval(() => {
   rotation += 1
   square.style.transform = `rotate(${rotation}deg)`
   if (rotation >= MAX_ROTATION) {
     clearInterval(intervalId)
   }
-}, 1000/10)
+}, 1000/10)*/
