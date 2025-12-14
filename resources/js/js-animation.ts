4d0f5fe2e1686d4935a827a633712f6dfb65c0d3
petrero <petrero@gmail.com>
2025-12-14 15:26:35 +0200

    19.5 And so while set timeout only runs once, there is another function called set interval, which is going to run over and over. And so that gives us a way to progressively change this rotation angle. So why don't we have a let rotation equals zero, which is a rotation angle. And then instead of hard coding the 45 degree value here we are going to say rotation equals rotation plus five. So we add five degrees to the rotation and here we can transform this to a template string so that we can dynamically pass the rotation value. All right, so now every second, the rotation angle should increase by five and the styles should be updated. Let's go take a look. Alright. And it seems to be working every second. The square is rotated by five degrees. Uh, and this is the starting, uh, of a animation if you want.

--- resources/js/js-animation.ts
+++ resources/js/js-animation.ts
@@ -2,6 +2,9 @@ const square = document.querySelector('.square')
 
 square.style.transform = 'rotate(45deg)'
 
-setTimeout(() => {
-  square.style.transform = 'rotate(45deg)'
+let rotation = 0
+
+setInterval(() => {
+  rotation += 5
+  square.style.transform = `rotate(${rotation}deg)`
 }, 1000)
